0984fceb774d25dfb51ddf178ce59eb3002b4c54
noswear94 <piyushav94@gmail.com>
2024-05-15 02:01:33 +0530

    Add mascot image to marketing header and wrap app layout with ClerkProvider
    * Adds an Image component for a mascot in the marketing header
    * Wraps the app layout with ClerkProvider for Clerk authentication

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -8,6 +8,7 @@ type Props = {
 
 const MarketingLayout = ({ children}: Props) =>{
     return(
+        
         <div className="min-h-screen flex flex-col">
             <Header/>
             <main className="flex-1 flex flex-col items-center justify-center">
